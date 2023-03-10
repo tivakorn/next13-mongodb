b447c466f8c28a773fd4ff18e2df0a6b9dc2075a
tivakorn <tivakorn@Tivakorns-MacBook-Pro.local>
2023-03-11 01:18:55 +0700

    test.

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,20 @@ import * as ModelsMembers from '../models/members'
 
 const HomePage = async () => {
 
-  const members = await ModelsMembers.getMembersList()
+  // const members = await ModelsMembers.getMembersList()
 
-  const renderMemberList = () => {
+  // const renderMemberList = () => {
 
-    return members.map((item, key) => {
+  //   return members.map((item, key) => {
 
-      return (
-        <h1 key={key}>{item.name}</h1>
-      )
-    })
-  }
+  //     return (
+  //       <h1 key={key}>{item.name}</h1>
+  //     )
+  //   })
+  // }
   return (
     <div>
-      {renderMemberList()}
+      {'test'}
     </div>
   )
 }
