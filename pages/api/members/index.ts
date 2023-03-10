fbfc98d1581bb03d393c506746e81e02ff4d97e6
tivakorn <tivakorn@Tivakorns-MacBook-Pro.local>
2023-03-11 02:02:04 +0700

    connect db for vercel

--- pages/api/members/index.ts
+++ pages/api/members/index.ts
@@ -1,12 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
 // Models
-// import * as ModelsMembers from '../../../models/members'
+import * as ModelsMembers from '../../../models/members'
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-    // const memberList = await ModelsMembers.getMembersList()
+    const memberList = await ModelsMembers.getMembersList()
 
-    res.status(200).json({ data: 'memberList' })
+    res.status(200).json({ data: memberList })
 }
