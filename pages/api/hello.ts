e40fe2cf959583d2f9fa6b40929cf8ab60c9c5b3
tivakorn <tivakorn@Tivakorns-MacBook-Pro.local>
2023-03-11 01:37:57 +0700

    test api pages

--- pages/api/hello.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiResponse } from "next"
-
-export default (req: NextApiResponse, res: NextApiResponse) => {
-
-    res.json({ name: 'tivakorn'})
-}
---
+++ pages/api/members/index.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+// Models
+import * as ModelsMembers from '../../../models/members'
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    const memberList = await ModelsMembers.getMembersList()
+
+    res.status(200).json({ data: memberList })
+}
